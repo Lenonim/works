98c6d13d9198828b9a3d28ea58a986e457d1afc4
Lenonim <ilja.ist.am.bestem@mail.ru>
2023-02-12 23:04:11 +0300

    add utf reader on cpp

---
+++ CppProgs/UTF8_Reader/TestSTl.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <locale>
+#include <string>
+#include <codecvt>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<std::u32string> lines;
+    std::vector<char32_t> line;
+
+    std::string file_name;
+    std::cout << "Enter the file name: ";
+    std::getline(std::cin, file_name);
+
+    std::locale loc(std::locale(), new std::codecvt_utf8<char32_t>);
+    std::basic_ifstream<char32_t> ifs(file_name, std::ios::in);
+    ifs.imbue(loc);
+
+    while (!ifs.eof())
+        line.push_back(ifs.get());
+
+    auto left_it = std::find(begin(line), end(line), char32_t('['));
+    while (left_it != end(line)) {
+        auto right_it = std::find(left_it, end(line), char32_t(']'));
+        if (right_it == end(line))
+            break;
+        line.erase(left_it, right_it + 1);
+        left_it = std::find(begin(line), end(line), char32_t('['));
+    }
+
+    std::basic_ofstream<char32_t> ofs("new_" + file_name, std::ios::out);
+    ofs.imbue(loc);
+    for(auto element : line)
+        ofs << element;
+    ofs.close();
+
+    return 0;
+}
