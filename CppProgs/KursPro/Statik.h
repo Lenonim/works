be027f10ddb6fcd90b3c802e8538ffd978baa2cc
Lenonim <ilja.ist.am.besten@mail.ru>
2023-06-28 19:27:41 +0300

    тест редми

--- CppProgs/KursPro/Statik.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "lender.h" // подключение структуры
-
-void SreibenDaten(lander*& land , int& Kapazitat); // вывод записей 
-void WahlDaten(lander*& land , int& Kapazitat); // выборка данных
--- CppProgs/KursPro/Suchen.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-#include "lender.h" // подключение структуры
-
-void SuchenDaten(lander*& land , int& Kapazitat); // поиск данных 
-int SuchenFurSubstragierung(lander*& land , int& Kapazitat); // поиск для удаления 
